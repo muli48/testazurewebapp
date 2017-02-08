ffcdbca83b2cb9310aef3d5bfba8a36750a721f4
Martin Müller <muli48@gmail.com>
2017-02-08 20:24:08 +0100

    some text change, added webapi

---
+++ testazurewebapp/TestWebApi/Controllers/ValuesController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace TestWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class ValuesController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var serverName = Environment.MachineName;
+            var processors = Environment.ProcessorCount;
+            return new string[] { serverName, processors.ToString() };
+        }
+    }
+}
